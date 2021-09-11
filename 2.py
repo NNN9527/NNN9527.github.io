41634e32c41e278205762ed0c969292ac1a609d0
NNN9527 <86363868+NNN9527@users.noreply.github.com>
2021-09-11 19:06:49 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+import cv2
 import time
 
 import streamlit.components.v1 as components
@@ -17,8 +17,15 @@ st.time_input('某时')
 
 uploaded_file = st.file_uploader('我们跑路啦！！！！！', type="png")
 
+if uploaded_file is not None:
+    # Convert the file to an opencv image.
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    opencv_image = cv2.imdecode(file_bytes, 1)
 
-color = st.color_picker('选择心动色彩', '#64C3FB')
+    # Now do something with the image! For example, let's display it:
+    st.image(opencv_image, channels="BGR")
+
+
 
 
 # Add a placeholder 
@@ -50,12 +57,10 @@ st.write('三点几啦，该起床读书啦！！！', color)
 st.button('点我开启时间循环！！！！！')
 
 '做然啊做，老板不会心疼你啦，摸鱼先'
-# embed streamlit docs in a streamlit app
-components.iframe("https://www.bilibili.com/video/BV1i5411K7cY?spm_id_from=333.851.b_7265636f6d6d656e64.1"
-                  ,height=600,width=730, )
 
-st.title('帅！')
-st.write('大帅比！！')
+
+st.title('9527NNN帅！')
+st.write('9527NNN大帅比！！')
 st.write("下面是一个表格的尝试:")
 st.write(pd.DataFrame({
     '520': [1, 3, 1,4],
@@ -96,14 +101,11 @@ if genre == '甜的':
 else:
  st.write("亚希拉你")
 
-st.markdown('** _really_ cool**.')
+st.markdown('**9527NNN _really_ cool**.')
 
 components.iframe("https://nnn9527.github.io"
-                  ,height=600,width=730, )
+                  ,height=600,width=400, )
 
-st.balloons()
-st.balloons()
-st.balloons()
 
 
 code = '''def hello():
@@ -111,35 +113,60 @@ code = '''def hello():
 st.code(code, language='python')
 
 
-def sidebar():
-    df_parameters = pd.DataFrame({'industry': ['-','开','关'],'name': ['-','A','B'],'time': ['-','5天','7天']})
+def sidebar():#左边菜单控制栏0
+    df_parameters = pd.DataFrame({'function': ['-','游戏','小说','视频'],'tool': ['-','半小时','一小时','自定义']})
     select_parameters=[]
-    option_industry = '-'
-    option_name = '-'
-    option_time = '-'
+    option_function = '游戏'
+    option_function = '小说'
+    option_function = '视频'
+    option_time = '自定义'
+    option_time = '半小时'
+    option_time = '一小时'
+    option_time = '      '
     
-    option_industry = st.sidebar.selectbox('请选择股票行业',df_parameters['industry'])
-    if option_industry != '-':
-        select_parameters.append(option_industry)
-        option_name = st.sidebar.selectbox('请选择股票名称',df_parameters['name'])
-    else:
-        pass
+    option_function = st.sidebar.selectbox('请选择娱乐',df_parameters['function'])
+    select_parameters.append(option_function)
+    option_time = st.sidebar.selectbox('请选择工具',df_parameters['tool']) 
+
+    if option_function =='游戏':
+          '游戏'
+    else:pass
+    if option_function =='小说':
+          '小说'
+    else:pass
+    if option_function =='视频':
+         components.iframe("https://www.bilibili.com/video/BV1i5411K7cY?spm_id_from=333.851.b_7265636f6d6d656e64.1"
+                  ,height=600,width=400, )
+    else:pass
+
+    if option_time =='半小时':
+          '半小时'
+    else:pass
+    if option_time =='一小时':
     
-    if option_name!= '-':
-        select_parameters.append(option_name)
-        option_time = st.sidebar.selectbox('请选择时间窗口',df_parameters['time'])
-    else:
-        pass
-        
-    if option_time!= '-':
-        select_parameters.append(option_time)
-        version_option=st.sidebar.selectbox('选择历史版本：',('-','版本1', '版本2', '版本3'))
-        st.sidebar.button('切换页面')
-        st.sidebar.button('更新参数')
-        
+          '一小时'
+    else:pass
+    if option_time =='自定义':
+      zidingyi1_time= st.sidebar.text_input('     ', '请输入视频时间/小时')
+      zidingyi2_time= st.sidebar.text_input('     ', '请输入视频时间/分钟')
+      if zidingyi1_time != ('请输入视频时间/小时') and zidingyi2_time != ('请输入视频时间/分钟'):
+       st.sidebar.write('游戏始视频')
+       st.sidebar.write(zidingyi1_time,'小时')
+       st.sidebar.write(zidingyi2_time,'分钟')
+      else:pass
+    else:pass
+    left_column, right_column = st.sidebar.beta_columns(2)
+    guanyu=left_column.button('关于')
+    with right_column:
+     bangzhu=st.button('帮助')
+     if guanyu:
+      st.write("作者是9527NNN")
+     else:pass
+     if bangzhu:
+      st.write("这里没有任何可以帮您的地方")
     return select_parameters
 
-sidebar=sidebar()
+sidebar()
 
 st.markdown('### 三次方计算器 :sunglasses:')
 x = st.slider('输入一个数字')
