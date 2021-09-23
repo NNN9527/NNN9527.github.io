51a42f2494f380ddd7ba9b5a8cd87c620b79d579
NNN9527 <86363868+NNN9527@users.noreply.github.com>
2021-09-23 22:24:17 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+import cv2
 import time
 
 import streamlit.components.v1 as components
@@ -9,100 +9,110 @@ import streamlit.components.v1 as components
 
 
 
+def shigongjidi():
+  st.title('Welcome to 摸鱼圣地！！！')
+  st.date_input('某天')
+  st.time_input('某时')
 
-st.title('Welcome to 摸鱼圣地！！！')
+  uploaded_file = st.file_uploader('我们跑路啦！！！！！', type="png")
 
-st.date_input('某天')
-st.time_input('某时')
+  if uploaded_file is not None:
+     # Convert the file to an opencv image.
+     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+     opencv_image = cv2.imdecode(file_bytes, 1)
 
+     # Now do something with the image! For example, let's display it:
+     st.image(opencv_image, channels="BGR")
 
+   # Add a placeholder 
+  latest_iteration = st.empty()
+  bar = st.progress(0)
 
+  for i in range(3):
+    # Update the progress bar with each iteration.
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
 
-
-
-# Add a placeholder 
-latest_iteration = st.empty()
-bar = st.progress(0)
-
-for i in range(3):
-  # Update the progress bar with each iteration.
-  latest_iteration.text(f'Iteration {i+1}')
-  bar.progress(i + 1)
-  time.sleep(0.1)
-
-st.write('欢迎来到跑路天堂9527！！！！！！')
-for i in range(3):
-  # Update the progress bar with each iteration.
-  latest_iteration.text(f'Iteration {i+1}')
-  bar.progress(i + 1)
-  time.sleep(0.1)
+  st.write('欢迎来到跑路天堂9527！！！！！！')
+  for i in range(3):
+    # Update the progress bar with each iteration.
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
   
-st.write('早上好，打工人！！！')
-for i in range(3):
-  # Update the progress bar with each iteration.
-  latest_iteration.text(f'Iteration {i+1}')
-  bar.progress(i + 1)
-  time.sleep(0.1)
-st.write('三点几啦，该起床读书啦！！！')
+  st.write('早上好，打工人！！！')
+  for i in range(3):
+    # Update the progress bar with each iteration.
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+  st.write('三点几啦，该起床读书啦！！！')
 
 
-st.button('点我开启时间循环！！！！！')
+  st.button('点我开启时间循环！！！！！')
 
-'做然啊做，老板不会心疼你啦，摸鱼先'
-
-
-st.title('9527NNN帅！')
-st.write('9527NNN大帅比！！')
-st.write("下面是一个表格的尝试:")
-st.write(pd.DataFrame({
-    '520': [1, 3, 1,4],
-    '9527': [7, 0, 8, 6]
-}))
-"""
-# just a magit
-Here's NO ONE :
-"""
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
-
-st.write('三呢？？？？？？？？？')
-
-st.write('为啥加不了超霸气的巨大字体')
-st.write('**我也不知道什么原理的地图**')
-st.write('为啥加粗时灵时不灵')
+  '做然啊做，老板不会心疼你啦，摸鱼先'
 
 
-if st.checkbox('看不到我！！！！！'):
- 11
-else:pass
+  st.title('9527NNN帅！')
+  st.write('9527NNN大帅比！！')
+  st.write("下面是一个表格的尝试:")
+  st.write(pd.DataFrame({
+      '520': [1, 3, 1,4],
+      '9527': [7, 0, 8, 6]
+  }))
+  """
+  # just a magit
+  Here's NO ONE :
+  """
+  df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+  })
 
-columns=['a', 'b', 'c']
-option = st.selectbox(
-    '《豆腐吃咸的还是甜的？》',
-     df['second column']
-    )
-'甜党天下无敌！！！！！！！', option
+  st.write('三呢？？？？？？？？？')
+
+  st.write('为啥加不了超霸气的巨大字体')
+  st.write('**我也不知道什么原理的地图**')
+  st.write('为啥加粗时灵时不灵')
 
 
-genre = st.radio( "《豆腐吃咸的还是甜的？》",
-('甜的', '甜的', '咸的'))
-if genre == '甜的':
- st.write('甜党天下第一！！！！！！！')
-else:
- st.write("亚希拉你")
+  if st.checkbox('看不到我！！！！！'):
+   11
+  else:pass
 
-st.markdown('**9527NNN _really_ cool**.')
+  columns=['a', 'b', 'c']
+  option = st.selectbox(
+      '《豆腐吃咸的还是甜的？》',
+       df['second column']
+      )
+  '甜党天下无敌！！！！！！！', option
 
-components.iframe("https://nnn9527.github.io"
+
+  genre = st.radio( "《豆腐吃咸的还是甜的？》",
+  ('甜的', '甜的', '咸的'))
+  if genre == '甜的':
+   st.write('甜党天下第一！！！！！！！')
+  else:
+   st.write("亚希拉你")
+
+  st.markdown('**9527NNN _really_ cool**.')
+
+  components.iframe("https://nnn9527.github.io"
                   ,height=600,width=400, )
 
+  code = '''def hello():
+  ...     print("Hello, Streamlit!")'''
+  st.code(code, language='python')
+
+  st.markdown('### 三次方计算器 :sunglasses:')
+  x = st.slider('输入一个数字')
+  st.write(x, '的2次方为：', x**2)
+  st.markdown('> Streamlit挺好用 :+1:')
 
 
-code = '''def hello():
-...     print("Hello, Streamlit!")'''
-st.code(code, language='python')
+
 
 
 def sidebar():#左边菜单控制栏0
@@ -124,11 +134,12 @@ def sidebar():#左边菜单控制栏0
           '游戏'
     else:pass
     if option_function =='小说':
-          '小说'
+         components.iframe("https://m.ddyueshu.com/wapbook/11508_655702269_3.html"
+                  ,height=3600,width=400, )
     else:pass
     if option_function =='视频':
          components.iframe("https://www.bilibili.com/video/BV1i5411K7cY?spm_id_from=333.851.b_7265636f6d6d656e64.1"
-                  ,height=600,width=400, )
+                  ,height=600,width=700, )
     else:pass
 
     if option_time =='半小时':
@@ -148,6 +159,14 @@ def sidebar():#左边菜单控制栏0
       else:pass
     else:pass
     left_column, right_column = st.sidebar.beta_columns(2)
+    wenghao=left_column.button('????')
+    with right_column:
+     zaxiang=st.button('杂项')
+     if wenghao:
+        '??????'
+    if zaxiang:
+         shigongjidi()
+    left_column, right_column = st.sidebar.beta_columns(2)
     guanyu=left_column.button('关于')
     with right_column:
      bangzhu=st.button('帮助')
@@ -159,12 +178,6 @@ def sidebar():#左边菜单控制栏0
     return select_parameters
 
 sidebar()
-
-st.markdown('### 三次方计算器 :sunglasses:')
-x = st.slider('输入一个数字')
-st.write(x, '的2次方为：', x**2)
-st.markdown('> Streamlit挺好用 :+1:')
-
 
 
 
